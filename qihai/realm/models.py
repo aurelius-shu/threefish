2ed88745fa2269af681e199e20a689008e936c0a
aurelius-shu <aureliusshu@gmail.com>
2020-05-13 23:49:27 +0800

    set realm data source, language, timezone add model: user exec migrate

--- qihai/realm/models.py
+++ qihai/realm/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    def __str__(self):
+        return (self.nickname or self.username) + (('<%s>' % self.email) if self.email else '')
+
+    username = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=128)
+    reg_time = models.DateTimeField('time registered')
+    last_login = models.DateTimeField('time last login')
+    status = models.BooleanField('active or inactive')
