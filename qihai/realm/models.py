fd3c832ba9ced6616f00b40ed273d412b797ee52
aurelius-shu <aureliusshu@gmail.com>
2020-05-18 17:42:25 +0800

    add model: Image add model: Article

--- qihai/realm/models.py
+++ qihai/realm/models.py
@@ -26,3 +26,34 @@ class User(models.Model):
     reg_time = models.DateTimeField('time registered')
     last_login = models.DateTimeField('time last login')
     is_active = models.BooleanField('active or inactive')
+
+
+class Image(models.Model):
+    """
+    图片资源
+    """
+
+    def __str__(self):
+        return '<%s>%s' % (self.upload_time, self.filename)
+
+    md5_key = models.CharField(max_length=36)
+    filename = models.CharField(max_length=50)
+    upload_user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    upload_time = models.DateTimeField('time upload')
+    image_base64 = models.TextField()
+
+
+class Article(models.Model):
+    """
+    文章
+    """
+
+    def __str__(self):
+        return '<%s>%s' % (self.publish_time, self.title)
+
+    title = models.CharField(max_length=100)
+    comment = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL)
+    publish_time = models.DateTimeField('time published')
+    content = models.TextField()
