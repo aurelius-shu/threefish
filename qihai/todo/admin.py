9ed72f910141dc2f69d3139cfa96115be20093b7
aurelius-shu <aureliusshu@gmail.com>
2020-09-30 13:50:10 +0800

    add todo admin: project

--- qihai/todo/admin.py
+++ qihai/todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from todo.models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('base info', {'fields': ['name', 'type', 'status', 'owner', 'create_time', 'comment']})
+    ]
+    list_display = (
+        'name', 'type', 'status', 'owner', 'create_time', 'update_time', 'comment'
+    )
+    ordering = ['-update_time']
+    list_filter = ['type', 'status']
+    search_fields = ['name', 'create_time', 'comment']
+
+
+admin.site.register(Project, ProjectAdmin)
+
+admin.site.site_title = 'qihai'
+
+admin.site.site_header = 'to-do'
