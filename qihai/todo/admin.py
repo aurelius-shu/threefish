747daed0d3cc757202fda22b3f41c21f9ca7c3a3
aurelius-shu <aureliusshu@gmail.com>
2020-09-30 16:02:19 +0800

    add admin: task, schedule, step

--- qihai/todo/admin.py
+++ qihai/todo/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from todo.models import Project
+from todo.models import Project, Task, Schedule, Step
 
 
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('base info', {'fields': ['name', 'type', 'status', 'owner', 'create_time', 'comment']})
+        ('base info', {'fields': ['name', 'type', 'status', 'owner', 'comment']})
     ]
     list_display = (
         'name', 'type', 'status', 'owner', 'create_time', 'update_time', 'comment'
@@ -14,8 +14,46 @@ class ProjectAdmin(admin.ModelAdmin):
     search_fields = ['name', 'create_time', 'comment']
 
 
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('base info', {'fields': ['keyword', 'project', 'status', 'description', ]})
+    ]
+    list_display = (
+        'project', 'keyword', 'status', 'create_time', 'update_time', 'description',
+    )
+    ordering = ['-update_time']
+    list_filter = ['project', 'status', 'keyword']
+    search_fields = ['keyword', 'create_time', 'description']
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('base info', {'fields': ['keyword', 'status', 'start_time', 'end_time', 'comment', ]})
+    ]
+    list_display = (
+        'keyword', 'status', 'create_time', 'update_time', 'start_time', 'end_time', 'comment',
+    )
+    ordering = ['-update_time']
+    list_filter = ['keyword', 'status', 'create_time', 'comment']
+    search_fields = ['keyword', 'status', 'create_time', 'comment']
+
+
+class StepAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('base info', {'fields': ['schedule', 'task', 'status', 'start_time', 'end_time', 'outcome', ]})
+    ]
+    list_display = (
+        'schedule', 'task', 'status', 'create_time', 'update_time', 'start_time', 'end_time', 'outcome',
+    )
+    ordering = ['-update_time']
+    list_filter = ['schedule', 'task', 'status', 'create_time', 'outcome']
+    search_fields = ['schedule', 'task', 'status', 'create_time', 'outcome']
+
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Step, StepAdmin)
 
 admin.site.site_title = 'qihai'
-
 admin.site.site_header = 'to-do'
--- qihai/todo/models.py
+++ qihai/todo/models.py
@@ -25,36 +25,48 @@ class Project(models.Model):
     type = models.IntegerField(default=0, choices=[(tag.value, tag.name) for tag in ProjectType])
     status = models.IntegerField(default=1, choices=[(tag.value, tag.name) for tag in Status])
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='projects')
-    create_time = models.DateTimeField(default=timezone.now)
+    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     comment = models.CharField(max_length=255, default=' ')
+
+    def __str__(self):
+        return f"{ProjectType(self.type).name} | {self.name}"
 
 
 class Task(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
     keyword = models.CharField(max_length=30, default=' ')
     status = models.IntegerField(default=1, choices=[(tag.value, tag.name) for tag in Status])
-    create_time = models.DateTimeField(default=timezone.now)
+    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     description = models.CharField(max_length=255, default=' ')
+
+    def __str__(self):
+        return f"{self.project.name} | {self.keyword}"
 
 
 class Schedule(models.Model):
     keyword = models.CharField(max_length=30, default=' ')
     status = models.IntegerField(default=1, choices=[(tag.value, tag.name) for tag in Status])
-    create_time = models.DateTimeField(default=timezone.now)
+    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(default=timezone.now)
     comment = models.CharField(max_length=255, default=' ')
+
+    def __str__(self):
+        return f"{self.start_time.astimezone(timezone.get_current_timezone()).strftime('%m-%d %H:%M')} -> {self.end_time.astimezone(timezone.get_current_timezone()).strftime('%m-%d %H:%M')} | {self.keyword}"
 
 
 class Step(models.Model):
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, related_name='steps')
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='steps')
     status = models.IntegerField(default=1, choices=[(tag.value, tag.name) for tag in Status])
-    create_time = models.DateTimeField(default=timezone.now)
+    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(default=timezone.now)
     outcome = models.CharField(max_length=255, default=' ')
+
+    def __str__(self):
+        return f"{self.start_time.astimezone(timezone.get_current_timezone()).strftime('%m-%d %H:%M')} -> {self.end_time.astimezone(timezone.get_current_timezone()).strftime('%m-%d %H:%M')}"
